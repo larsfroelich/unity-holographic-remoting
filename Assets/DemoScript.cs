6025039fec7ef550ee4de18535c4575451bb5105
Lars Frölich <lars.froelich@gmail.com>
2019-03-12 16:11:34 +0100

    added initial version of unity demo-project

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Input;
+
+public class DemoScript : MonoBehaviour {
+    public string hololensIP;
+    public GameObject cube;
+
+    private bool _blueCube;
+    private bool _connected;
+    private GestureRecognizer _gestureRecognizer;
+
+    private void Start() { HolographicRemoting.Connect(hololensIP); }
+
+    private void Update() {
+        if (HolographicRemoting.ConnectionState
+            == HolographicStreamerConnectionState.Connected
+            && !_connected) {
+            _connected = true; // run a single time upon connection established
+            
+            _gestureRecognizer = new GestureRecognizer();
+            _gestureRecognizer.Tapped += args => {
+                _blueCube = !_blueCube;
+                cube.GetComponent<MeshRenderer>().material.color = _blueCube ? Color.blue : Color.magenta;
+            };
+        
+            _gestureRecognizer.StartCapturingGestures();
+            _gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+        }
+    }
+}
